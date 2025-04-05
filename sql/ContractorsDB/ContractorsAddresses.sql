46f78fb43294bbbb1022b481fffb5ef933058dbf
kruzo7@gmail.com <kruzo7@gmail.com>
2025-04-05 16:22:42 +0200

    typo

--- sql/ContractorsDB/ContractorsAddresses.sql
+++ sql/ContractorsDB/ContractorsAddresses.sql
@@ -1,8 +1,8 @@
 CREATE TABLE [dbo].[ContractorsAddresses]
 (
-  [ContractorAddresseId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
+  [ContractorAddressId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
   [ContractorID] INT NOT NULL,
-  [ContractorAddresseLine] NVARCHAR(1024) NOT NULL,
+  [ContractorAddressLine] NVARCHAR(1024) NOT NULL,
 
   CONSTRAINT FK_ContractorsAddresses_Contractors FOREIGN KEY (ContractorID)
         REFERENCES Contractors (ContractorID)        
