a1b285e133f6bfa460b82213a71565279049cc06
kruzo7@gmail.com <kruzo7@gmail.com>
2025-04-07 10:02:16 +0200

    add index

--- sql/ContractorsDB/Contractors.sql
+++ sql/ContractorsDB/Contractors.sql
@@ -1,7 +1,7 @@
 CREATE TABLE [dbo].[Contractors]
 (
   [ContractorId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
-  [ContractorName] NVARCHAR(1024) NOT NULL,
-  [ContractorNIP] NUMERIC(10, 0) NOT NULL,
+  [ContractorName] NVARCHAR(1024) NOT NULL INDEX IX_Contractors_ContractorName NONCLUSTERED,
+  [ContractorNIP] NUMERIC(10, 0) NOT NULL INDEX IX_Contractors_ContractorNIP NONCLUSTERED,
   [ContractorREGON] NUMERIC(14, 0) NOT NULL
 )
