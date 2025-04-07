b709a83affccb99b9181dd2459713727666f98da
kruzo7@gmail.com <kruzo7@gmail.com>
2025-04-07 10:16:47 +0200

    UNIQUE values.

--- sql/ContractorsDB/Contractors.sql
+++ sql/ContractorsDB/Contractors.sql
@@ -2,6 +2,6 @@ CREATE TABLE [dbo].[Contractors]
 (
   [ContractorId] INT NOT NULL PRIMARY KEY IDENTITY(1,1),
   [ContractorName] NVARCHAR(1024) NOT NULL INDEX IX_Contractors_ContractorName NONCLUSTERED,
-  [ContractorNIP] NUMERIC(10, 0) NOT NULL INDEX IX_Contractors_ContractorNIP NONCLUSTERED,
-  [ContractorREGON] NUMERIC(14, 0) NOT NULL
+  [ContractorNIP] NUMERIC(10, 0) NOT NULL UNIQUE INDEX IX_Contractors_ContractorNIP NONCLUSTERED,
+  [ContractorREGON] NUMERIC(14, 0) NOT NULL UNIQUE
 )
